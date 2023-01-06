01681d1f3c872bc35b5740c19fffc6f5602200b3
Enbi81 <wotng2002@gmail.com>
2023-01-06 17:46:37 +0100

    adding json derived type attributes

---
+++ Parser/MidLevel/IJsonValue.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+using Parser.Analyzer.Enums;
+
+namespace Parser.JsonValues;
+
+[JsonDerivedType(typeof(ArrayValue))]
+[JsonDerivedType(typeof(BoolValue))]
+[JsonDerivedType(typeof(NullValue))]
+[JsonDerivedType(typeof(NumberValue))]
+[JsonDerivedType(typeof(ObjectValue))]
+[JsonDerivedType(typeof(StringValue))]
+public interface IJsonValue
+{
+    public string? Name { get; }
+    public string FullPath { get; }
+    public JsonValueType Type { get; }
+    public int Depth { get; }
+    public JsonMemberType JsonMemberType { get; }
+}
