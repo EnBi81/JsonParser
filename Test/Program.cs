a4580de347ce19ef124d4d5fe6c9bb8e77e1de81
Enbi81 <wotng2002@gmail.com>
2023-01-06 17:47:11 +0100

    modifying sandbox

--- Test/Program.cs
+++ Test/Program.cs
@@ -3,33 +3,96 @@
 using System;
 using System.IO;
 using System.Net.Http;
+using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Parser;
 using Parser.JsonValues;
 
-var file = @"C:\Users\enbi8\Downloads\test.json";
-var largeLink = "https://raw.githubusercontent.com/zemirco/sf-city-lots-json/master/citylots.json";
 
-Stream FromFile(string file)
+string testJson = """
 {
+	
+	"character": "varv\\"
+	"obj": {
+		"randkey": "hello12345\"",
+		"second": {
+			"hi": 2
+		}
+	},
+	"hundredIterator": [
+		0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60
+, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114,
+ 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 1
+61, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197, 198, 199 
+	]
+	"tomb": [{
+		"hello": "world",
+		"multi": [{
+			"multiProp": "hi"
+		}]
+	},
+	{   
+		"hello": "vilag"
+	}],
+	"iterator2": [{
+		"heyJo": "this is a new iterator"
+	}]
+}
+""";
+
+Stream FromFile()
+{
+    string file = @"C:\Users\enbi8\Downloads\test.json";
     return File.OpenRead(file);
 }
 
-async Task<Stream> FromWeb(string link)
+async Task<Stream> FromWeb()
 {
-    HttpClient httpClient = new HttpClient();
-    var response = await httpClient.GetStreamAsync(new Uri(link));
+    string link = "https://raw.githubusercontent.com/zemirco/sf-city-lots-json/master/citylots.json";
+    var httpClient = new HttpClient();
+    Stream response = await httpClient.GetStreamAsync(new Uri(link));
     return response;
 }
 
-//var stream = await FromWeb(largeLink); 
-var stream = FromFile(file);
+Stream stream = new MemoryStream(Encoding.UTF8.GetBytes(testJson));
+//Stream stream = await FromWeb(); 
+//Stream stream = FromFile();
 
+var jsonAnalyzer = new JsonAnalyzer(stream);
+IJsonValue analyzed = jsonAnalyzer.Analyze();
+
+var text = JsonSerializer.Serialize(analyzed, options: new JsonSerializerOptions
+{
+    WriteIndented = true
+});
+Print(text);
+return;
+
+
+Print("Number of iterators: " + jsonAnalyzer.Iterators.Count());
+foreach (ArrayValue iterator in jsonAnalyzer.Iterators)
+{
+    Print("Iterator: " + iterator.FullPath);
+    PrintArray(iterator);
+}
+
+
+Print();
+Print();
+
+// print the full analyzed tree
+if(analyzed is ObjectValue obj)
+    PrintObject(obj);
+
+
+
+/*
 var parser = new JsonDocumentIterator(stream);
 var rootElement = parser.Parse();
 var obj = (ObjectValue) rootElement;
 
-PrintObject(obj);
+PrintObject(obj);*/
 
 void Print(object? value = null)
 {
@@ -71,7 +134,7 @@ void PrintValue(IJsonValue value)
 
         return;
     }
-     Print();
+    Print();
 }
 
 // print out an array
